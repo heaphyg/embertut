7717c9e790d9d5bf1c18f7b61fcc762d1979e255
Greg Heaphy <heaphyg@gmail.com>
2014-05-19 22:25:03 -0500

    created the static mock up

---
+++ js/application.js
@@ -0,0 +1,1 @@
+window.Todos = Ember.Application.create(); // This will create a new instance of Ember.Application and make it available as a variable named Todos within your browser's JavaScript environment.
---
+++ js/router.js
@@ -0,0 +1,4 @@
+Todos.Router.map(function() {
+  this.resource('todos', { path: '/' });
+}); // This will tell Ember.js to detect when the application's URL matches '/' and to render the todos template.
+// make sure your understand what is defining the todo template
