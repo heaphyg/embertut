759d2c23f3db614dc2470ba777053f8c06c324e6
Greg Heaphy <heaphyg@gmail.com>
2014-05-22 22:51:00 -0500

    Instances of Todo can now be created in our todo list with logic from out todos_controller.js

---
+++ js/controllers/todos_controller.js
@@ -0,0 +1,37 @@
+//This controller will now respond to user action by using 
+//its newTitle property as the title of a new todo whose 
+//isCompleted property is false. Then it will clear its newTitle 
+//property which will synchronize to the template and reset the 
+//textfield. Finally, it persists any unsaved changes on the todo.
+
+// this is what controllers do - they have methods for models and save thos models or changes to those models to the data base
+// notice we had to add this dependency to the index.html file (script tags)
+Todos.TodosController = Ember.ArrayController.extend ({
+  actions: {
+    createTodo: function() {
+      //get the new todo title set by the "New Todo" text field
+      var title = this.get('newTitle');
+      if (!title.trim()) { return; } // I assume this returns the trimme version of the string if it isn't trim?? or does this return all the stuff written below?
+
+      // create the new Todo model // understand (this)
+      var todo = this.store.createRecord('todo', {
+        title: title,
+        isCompleted: false
+      });
+
+      // clear the "New Todo" text field
+      this.set('newTitle', '');
+
+      //save the new model
+      todo.save();
+    }
+  }
+});
+
+// how does the enter button trigger the action established above?? -- the enter key must be a built in default
+// also does hitting the enter button make an AJAX request
+
+
+// var str = "       Hello World!        ";
+// alert(str.trim());
+// Hello World!
--- js/router.js
+++ js/router.js
@@ -10,9 +10,33 @@ Todos.Router.map(function() {
 // implement a TodosRoute class with a model function that returns all the existing todos:
 Todos.TodosRoute = Ember.Route.extend({ 
   model: function() {
-    return this.store.find('todo'); // clarify what (this) refers to
+    return this.store.find('todo'); // clarify what (this) refers to -- i believe 'todo' in this case represents a particualr instance of the todo model
   }
 });
 
 //Because we hadn't implemented this class before (TodoRoute), Ember.js provided a Route for us with the default behavior of rendering a matching template named todos using its naming conventions for object creation.
 // how does the above associate with the fixtures in the todo.js file?
+
+// naming conventions model: Todo
+// template: data-template-name="todos"
+// controller: Todos.TodosController = Ember.ArrayController.extend({ .. });
+
+
+//In Ember.js, controllers allow you to decorate your models with 
+//display logic. In general, your models will have properties that
+// are saved to the server, while controllers will have properties 
+//that your app does not need to save to the server.
+
+//For example, if you were building a blog, you would have a BlogPost 
+//model that you would present in a blog_post template.
+
+// but we have a Todo model (singular) and todos template (plural??)
+// (Remember that a controller gets the model it represents from its route handler.)
+
+
+//This means that templates know about controllers and controllers 
+//know about models, but the reverse is not true. A model knows 
+//nothing about which (if any) controllers are decorating it, and 
+//controller does not know which views are presenting its properties.
+
+// TEMPLATE gets its properties from CONTOLLER -- CONTROLLER decorates a model (creates an instance of?)
