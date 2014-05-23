7d3e0b3185d7bffabbea18f302578a159f1337a8
Greg Heaphy <heaphyg@gmail.com>
2014-05-22 21:26:24 -0500

    todos are now striked through when isComplete attribute of Todo model is set to true

--- js/router.js
+++ js/router.js
@@ -2,7 +2,7 @@ Todos.Router.map(function() {
   this.resource('todos', { path: '/' });
 }); // This will tell Ember.js to detect when the application's URL matches '/' and to render the todos template.
 // make sure you understand what is defining the todo template. see line 9 of the html (data-template-name="todos")
-
+// do all templates go into the same html file??
 
 // In Ember, every route has an associated model. 
 
@@ -15,4 +15,4 @@ Todos.TodosRoute = Ember.Route.extend({
 });
 
 //Because we hadn't implemented this class before (TodoRoute), Ember.js provided a Route for us with the default behavior of rendering a matching template named todos using its naming conventions for object creation.
-// how does the above associate with the fictures in the todo.js file
+// how does the above associate with the fixtures in the todo.js file?
